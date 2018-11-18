bcd711fa069bd1409885012a6103d8ebe28e3818
Adrian Taylor <adrian@bromium.com>
2018-11-17 16:49:06 -0800

    Adding argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate clap; 
+use clap::App; 
+
 fn main() {
+    App::new("cpptors")
+       .version("1.0")
+       .about("Transpiles C++ to Rust. In theory.")
+       .author("Adrian Taylor")
+       .get_matches(); 
     println!("Hello, world!");
 }
